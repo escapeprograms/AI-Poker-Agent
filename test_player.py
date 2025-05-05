2754df6256a44e85a46b621fc9197acbf6969fc8
edfournier <ericfournier.d@gmail.com>
2025-05-05 00:25:53 -0400

    working search

--- test_player.py
+++ test_player.py
@@ -1,8 +1,7 @@
 import pprint
 
 from pypokerengine.players import BasePokerPlayer
-from search import minimax
-from pypokerengine.api.emulator import Emulator
+from search import minimax, manual_walk
 from pypokerengine.utils.card_utils import gen_cards
 from pypokerengine.utils.game_state_utils import attach_hole_card, attach_hole_card_from_deck, restore_game_state
 
@@ -15,18 +14,19 @@ class TestPlayer(BasePokerPlayer):
         seat = round_state["next_player"]
         uuid = round_state["seats"][seat]["uuid"]
 
-        print(valid_actions)
+        # TODO: use cheat deck in table constructor to control which cards get dealt?
 
-        # Clone game state
+        # Clone known game state
         game_state = restore_game_state(round_state)            
         game_state = attach_hole_card(game_state, uuid, gen_cards(hole_card))
+
+        # Generate random hole cards for opponents
         for seat in round_state["seats"]:
             if seat["uuid"] != uuid:
-                # Generate random hole cards
                 game_state = attach_hole_card_from_deck(game_state, seat["uuid"])
 
-        # Determine best action
-        return minimax(game_state, 2, True)[1]["action"]
+        # Search for bes action
+        return minimax(game_state, None, 2, True)[1]["action"]
 
     def receive_game_start_message(self, game_info): pass
     def receive_round_start_message(self, round_count, hole_card, seats): pass
